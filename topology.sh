7a6752018219f8f9c9dd80f420c73975652ce907
Colin Patrick Mccabe <cmccabe@alumni.cmu.edu>
2013-02-20 13:05:12 -0800

    2node changes
    Signed-off-by: Colin Patrick McCabe <cmccabe@cloudera.com>

--- topology.sh
+++ topology.sh
@@ -2,10 +2,12 @@
 
 while [ $# -gt 0 ]; do
     case $1 in
-        127.0.0.1)          LOC="/dc1/rack1";;
-        localhost)          LOC="/dc1/rack1";;
-        keter)              LOC="/dc1/rack1";;
-        *)                  LOC="/dc1/default";;
+        127.0.0.1)          LOC="/rack1";;
+        localhost)          LOC="/rack1";;
+        keter)              LOC="/rack1";;
+        haus02)                 LOC="/rack2";;
+        haus02.sf.cloudera.com) LOC="/rack2";;
+        *)                  LOC="/default";;
     esac
     echo -n "$LOC "
     shift
