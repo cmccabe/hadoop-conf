021316f1f1293c2f9d2766c87ad9cb51bb7d8f27
Colin Patrick Mccabe <cmccabe@cloudera.com>
2013-09-12 08:44:52 -0700

    delay before starting impalad
    Signed-off-by: Colin Patrick McCabe <cmccabe@cloudera.com>

--- start.sh
+++ start.sh
@@ -14,12 +14,12 @@ cd /home/cmccabe
 if [ "x$HOSTNAME" == "x${MASTER}" ]; then
     echo "starting master services..."
     /home/cmccabe/h/bin/hadoop namenode &> "${L}/namenode.log" &
-    impalad -state_store_host=${MASTER} -hostname=${HOSTNAME} -logtostderr &> "${L}/impalad.log" &
+    ( sleep 10s ; impalad -state_store_host=${MASTER} -hostname=${HOSTNAME} -logtostderr &> "${L}/impalad.log" ) &
     statestored -logtostderr &> "${L}/statestored.log" &
 else
     echo "starting slave services..."
     /home/cmccabe/h/bin/hadoop datanode &> "${L}/datanode.log" &
-    impalad -state_store_host=${MASTER} -hostname=${HOSTNAME} -logtostderr &> "${L}/impalad.log" &
+    ( sleep 10s ; impalad -state_store_host=${MASTER} -hostname=${HOSTNAME} -logtostderr &> "${L}/impalad.log" ) &
 fi
 echo "ready"
 disown
