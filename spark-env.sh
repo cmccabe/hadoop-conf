6401c68240deb5219cf3a62d2852fe4013e7ea1e
Colin Patrick Mccabe <cmccabe@cloudera.com>
2014-05-29 16:27:48 -0700

    spark-env.sh: adjust memory, add tachyon stuff
    Signed-off-by: Colin McCabe <cmccabe@alumni.cmu.edu>

--- spark-env.sh
+++ spark-env.sh
@@ -11,5 +11,11 @@ TACHYON_JAR=( ~/tachyon/main/target/tachyon-*-jar-with-dependencies.jar )
 TACHYON_JAR=${TACHYON_JAR[0]}
 export SPARK_CLASSPATH="$TACHYON_JAR:$SPARK_CLASSPATH"
 
-export SPARK_EXECUTOR_MEMORY=30720
-export SPARK_DRIVER_MEMORY=30720
+export SPARK_EXECUTOR_MEMORY=12288M
+export SPARK_DRIVER_MEMORY=12288M
+export TACHYON_JAVA_OPTS="-Dtachyon.worker.memory.size=512MB"
+
+# Make Tachyon accessible to Hadoop.
+export HADOOP_CLASSPATH="$TACHYON_JAR:$HADOOP_CLASSPATH"
+
+#-Dtachyon.worker.data.folder=
