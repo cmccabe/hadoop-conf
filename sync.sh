35d68aef76185bb91528d6c4eff8c3eeeb689e18
Colin Patrick Mccabe <cmccabe@cloudera.com>
2014-07-18 10:36:25 -0700

    sync.sh: sync ~/extra-native
    A place to put extra native libraries, like for snappyjava.  Hasn't
    quite solved the snappyjava problem yet, though.
    
    Signed-off-by: Colin Patrick McCabe <cmccabe@cloudera.com>

--- sync.sh
+++ sync.sh
@@ -4,6 +4,7 @@ if [[ `hostname` != "a2402.halxg.cloudera.com" ]]; then
     rsync --delete -avi a2402.halxg.cloudera.com:~/.bashrc-local ~/.bashrc-local 
     rsync --delete -avi a2402.halxg.cloudera.com:~/h/ ~/h/
     rsync --delete -avi --exclude logs a2402.halxg.cloudera.com:~/tachyon/ ~/tachyon/
+    rsync --delete -avi a2402.halxg.cloudera.com:~/extra-native/ ~/extra-native/
 
     # This last one might replace this very script.
     rsync --delete -avi a2402.halxg.cloudera.com:~/conf/ ~/conf 
