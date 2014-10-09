341af2a5f6cc95ee28ba50088f520c0a9537601a
Colin Patrick Mccabe <cmccabe@cloudera.com>
2014-10-09 16:59:20 -0700

    master-start.sh: refix zipkin invocation
    Signed-off-by: Colin Patrick McCabe <cmccabe@cloudera.com>

--- master-start.sh
+++ master-start.sh
@@ -6,8 +6,10 @@ LOG_DIR="/data/11/cmccabe/logs/"
 $B/hadoop namenode &> "${LOG_DIR}/namenode.log" &
 #$B/yarn resourcemanager &> "${LOG_DIR}/resource_manager.log" &
 #/home/cmccabe/tachyon/bin/tachyon-start.sh master &
-( cd /home/cmccabe/src/zipkin-1.1.0/ && ./bin/collector > "${LOG_DIR}/zipkin-collector.log" ) &
-( cd /home/cmccabe/src/zipkin-1.1.0/ && ./bin/query > "${LOG_DIR}/zipkin-query.log" ) &
-( cd /home/cmccabe/src/zipkin-1.1.0/ && ./bin/web > "${LOG_DIR}/zipkin-web.log" ) &
+pushd /home/cmccabe/src/zipkin-1.1.0/ 
+./bin/collector > "${LOG_DIR}/zipkin-collector.log" &
+./bin/query > "${LOG_DIR}/zipkin-query.log" &
+./bin/web > "${LOG_DIR}/zipkin-web.log" &
+popd
 
 disown
