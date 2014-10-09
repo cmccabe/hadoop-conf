f430b827da7015f4aa42f7014090c25de14205e3
Colin Patrick Mccabe <cmccabe@cloudera.com>
2014-10-09 15:26:30 -0700

    master-start.sh: fix zipkin start
    Signed-off-by: Colin Patrick McCabe <cmccabe@cloudera.com>

--- master-start.sh
+++ master-start.sh
@@ -6,8 +6,8 @@ LOG_DIR="/data/11/cmccabe/logs/"
 $B/hadoop namenode &> "${LOG_DIR}/namenode.log" &
 #$B/yarn resourcemanager &> "${LOG_DIR}/resource_manager.log" &
 #/home/cmccabe/tachyon/bin/tachyon-start.sh master &
-/home/cmccabe/src/zipkin-1.1.0/bin/collector > "${LOG_DIR}/zipkin-collector.log" &
-/home/cmccabe/src/zipkin-1.1.0/bin/query > "${LOG_DIR}/zipkin-query.log" &
-/home/cmccabe/src/zipkin-1.1.0/bin/web > "${LOG_DIR}/zipkin-web.log" &
+( cd /home/cmccabe/src/zipkin-1.1.0/ && ./bin/collector > "${LOG_DIR}/zipkin-collector.log" ) &
+( cd /home/cmccabe/src/zipkin-1.1.0/ && ./bin/query > "${LOG_DIR}/zipkin-query.log" ) &
+( cd /home/cmccabe/src/zipkin-1.1.0/ && ./bin/web > "${LOG_DIR}/zipkin-web.log" ) &
 
 disown
