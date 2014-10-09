b591d76796a37ee0de0e1235de48847b380c1f0d
Colin Patrick Mccabe <cmccabe@cloudera.com>
2014-10-09 12:45:31 -0700

    Add hadoop.trace.spanreceiver stuff
    Signed-off-by: Colin Patrick McCabe <cmccabe@cloudera.com>

--- master-start.sh
+++ master-start.sh
@@ -4,7 +4,10 @@ B="/home/cmccabe/h/bin"
 
 LOG_DIR="/data/11/cmccabe/logs/"
 $B/hadoop namenode &> "${LOG_DIR}/namenode.log" &
-$B/yarn resourcemanager &> "${LOG_DIR}/resource_manager.log" &
+#$B/yarn resourcemanager &> "${LOG_DIR}/resource_manager.log" &
 #/home/cmccabe/tachyon/bin/tachyon-start.sh master &
+/home/cmccabe/src/zipkin-1.1.0/bin/collector > "${LOG_DIR}/zipkin-collector.log" &
+/home/cmccabe/src/zipkin-1.1.0/bin/query > "${LOG_DIR}/zipkin-query.log" &
+/home/cmccabe/src/zipkin-1.1.0/bin/web > "${LOG_DIR}/zipkin-web.log" &
 
 disown
--- slave-start.sh
+++ slave-start.sh
@@ -3,7 +3,7 @@
 B="/home/cmccabe/h/bin"
 
 $B/hadoop datanode &> /data/11/cmccabe/logs/datanode.log &
-$B/yarn nodemanager &> /data/11/cmccabe/logs/nodemanager.log &
+#$B/yarn nodemanager &> /data/11/cmccabe/logs/nodemanager.log &
 #/home/cmccabe/tachyon/bin/tachyon-start.sh worker NoMount &
 
 disown
