fe9747ba5e0127deaca65f6980e8103254cddb39
Colin Patrick Mccabe <cmccabe@cloudera.com>
2015-02-23 16:05:11 -0800

    rename start-master.sh to start.sh and use daemontools
    Signed-off-by: Colin Patrick McCabe <cmccabe@cloudera.com>

---
+++ start.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -x
+
+launchit() {
+    name=$1
+    shift
+    env HADOOP_OPTS="-Dmy.hadoop.log.file=${name}.log" \
+        /usr/sbin/daemonize \
+            -p /data/11/cmccabe/pids/${name}.pid \
+            -l /data/11/cmccabe/pids/${name}.pid \
+            "${@}"
+}
+
+launch_htraced() {
+    env HTRACED_CONF_DIR=/home/cmccabe/conf \
+        /usr/sbin/daemonize \
+            -p /data/11/cmccabe/pids/htraced.pid \
+            -l /data/11/cmccabe/pids/htraced.pid \
+            /home/cmccabe/htrace/htrace-core/src/go/build/htraced
+}
+
+if [ $IS_MASTER -ne 0 ]; then
+    launchit namenode /home/cmccabe/sync/hadoop/bin/hadoop namenode
+    launch_htraced
+else
+    launchit datanode /home/cmccabe/sync/hadoop/bin/hadoop datanode
+fi
