9c6ee1515cb5ed023918529db3eb886756d444eb
Colin Patrick Mccabe <cmccabe@cloudera.com>
2015-09-09 16:08:26 -0700

    start.sh: start the YARN resourcemanager and nodemanager daemons

--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ launchit() {
     name=$1
     shift
     env HADOOP_OPTS="-Dmy.hadoop.log.file=${name}.log" \
+        YARN_OPTS="-Dmy.hadoop.log.file=${name}.log" \
         /usr/sbin/daemonize \
             -p /data/11/cmccabe/pids/${name}.pid \
             -l /data/11/cmccabe/pids/${name}.pid \
@@ -20,9 +21,14 @@ launch_htraced() {
             /home/cmccabe/htrace/htrace-htraced/go/build/htraced
 }
 
+# YARN won't log to /data/11/cmccabe/logs, but instead always wants to log in /home/cmccabe/sync/hadoop/logs 
+[ -e /home/cmccabe/sync/hadoop/logs ] || ln -s /data/11/cmccabe/logs /home/cmccabe/sync/hadoop/logs
+
 if [ $IS_MASTER -ne 0 ]; then
     launchit namenode /home/cmccabe/sync/hadoop/bin/hadoop namenode
+    launchit resourcemanager /home/cmccabe/sync/hadoop/bin/yarn resourcemanager
     launch_htraced
 else
     launchit datanode /home/cmccabe/sync/hadoop/bin/hadoop datanode
+    launchit nodemanager /home/cmccabe/sync/hadoop/bin/yarn nodemanager
 fi
